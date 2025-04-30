35016e2a9e90d31a99484d745404d65fa39a1ddb
lana-12 <lana.12@wanadoo.fr>
2025-04-30 15:10:46 +0200

    Review css on the page Project - admin  - collaborateur - home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
+use App\Repository\ProjectRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,10 +13,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(ProjectRepository $projectRepo, UserRepository $userRepo, EventRepository $eventRepo): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $templateData['counts'] = [
+                "projectsCount" => $projectRepo->count(),
+                "collaboratorsCount" => $userRepo->count(),
+                "eventsCount" => $eventRepo->count()
+            ];
+        return $this->render('home/index.html.twig', $templateData,
+        );
     }
 }
